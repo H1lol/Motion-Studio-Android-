6a3cbcd4efc0cdff5e352e701f26da35d2bfbe7d
H1lol <hasterahomuskurateraho73@gmail.com>
2026-09-13 10:27:40 +0530

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "MotionStudio"
+include(":app")
